9ce732942ab463e46152ffdd04cac3ba1558759c
jimy <jimyihendrix@gmail.com>
2021-07-19 14:29:42 +0200

    learnt how to introduce inputs in c

--- vars.c
+++ vars.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
+#include <stdbool.h> //incluye booleans
 
 int main(){
 
-    int integerA = 1;
-    float floatA = 1.1;
-    double doubleA = 1.2;
-    char letterA = 'A';
+    int integerA;
+    float floatA;
+    char letterA;
 
+    printf("ingresa el valor entero A: ");
+    scanf("%i", &integerA);
 
-    //imprime variable integer
-    printf("El valor de nuestro entero A es: %i, \nEl valor de nuestro flotante A es: %f, \nEl valor de nuestro double A es: %f, \nEl valor de nuestro char A es: %c", integerA, floatA, doubleA, letterA);
+    printf("\nIngresa el valor del float A: ");
+    scanf("%f", &floatA);
 
+    printf("\nIngresa el valor del char A: ");
+    scanf(" %c", &letterA);
 
-
+    printf("El entero A es: %i", integerA);
+    printf("El float A es: %f", floatA);
+    printf("El valor del char A es: %c", letterA);
 
     return 0;
 }
