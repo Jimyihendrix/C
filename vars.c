0a90a3b733ef95e8cb8ca4edf636259136cc840b
jimy <jimyihendrix@gmail.com>
2021-07-19 14:16:10 +0200

    vars learnt, next add input from keyboard

---
+++ vars.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main(){
+
+    int integerA = 1;
+    float floatA = 1.1;
+    double doubleA = 1.2;
+    char letterA = 'A';
+
+
+    //imprime variable integer
+    printf("El valor de nuestro entero A es: %i, \nEl valor de nuestro flotante A es: %f, \nEl valor de nuestro double A es: %f, \nEl valor de nuestro char A es: %c", integerA, floatA, doubleA, letterA);
+
+
+
+
+    return 0;
+}
